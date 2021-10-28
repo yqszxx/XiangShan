016c5acf2c0e1d7b6ee054df49c3ef7e8b5da098
Boyang Han <yqszxx@gmail.com>
2021-10-28 17:01:37 +0800

    Seperate trace of io and mems

--- src/main/scala/xstransforms/dumper.scala
+++ src/main/scala/xstransforms/dumper.scala
@@ -2,14 +2,10 @@ package xstransforms
 
 import chisel3.experimental.{ChiselAnnotation, RunFirrtlTransform, annotate}
 import firrtl.annotations.Annotation
-import firrtl.transforms.{DumperAnnotation, DumperTransform, Flatten, FlattenAnnotation}
+import firrtl.transforms.{Flatten, FlattenAnnotation}
 
 object Dumper {
   def dump(component: chisel3.Module): Unit = {
-    annotate(new ChiselAnnotation with RunFirrtlTransform {
-      def toFirrtl: Annotation = DumperAnnotation(component.toTarget)
-      def transformClass = classOf[DumperTransform]
-    })
     annotate(new ChiselAnnotation with RunFirrtlTransform {
       def toFirrtl: Annotation = FlattenAnnotation(component.toNamed)
       def transformClass = classOf[Flatten]
